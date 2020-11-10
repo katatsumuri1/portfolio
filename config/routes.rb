73010195564d369c696297df9c77587915e75a28
katatsumuri1 <styahuoku@gmail.com>
2020-11-10 18:16:04 +0000

    [add]header

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :tweets, only: [:index, :show, :create, :destroy, :new]
 
   root to: 'home#top'
+  get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
