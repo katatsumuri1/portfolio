74242aab56f57d24a63bfc4d8223ca221f360a0c
katatsumuri1 <styahuoku@gmail.com>
2020-11-14 06:35:12 +0000

    [add]コメント非同期通信

--- app/controllers/tweet_comments_controller.rb
+++ app/controllers/tweet_comments_controller.rb
@@ -1,16 +1,17 @@
 class TweetCommentsController < ApplicationController
   
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    comment = TweetComment.new(tweet_comment_params)
-    comment.user_id = current_user.id
-    comment.tweet_id = tweet.id
-    comment.save
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = TweetComment.new(tweet_comment_params)
+    @comment.user_id = current_user.id
+    @comment.tweet_id = @tweet.id
+    @comment.save
+    
   end
   
   def destroy
-    
-    TweetComment.find_by(id: params[:id], tweet_id: params[:tweet_id]).destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    TweetComment.find_by(id: params[:id],tweet_id: params[:tweet_id]).destroy
   end
   private
   def tweet_comment_params
