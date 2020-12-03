6cd1dc81efdf84f5cdc9158bc87a760cdb66f00c
katatsumuri1 <styahuoku@gmail.com>
2020-12-03 06:33:05 +0000

    [[fix]rankingページ修正]

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @random = Tweet.joins(:user).where.not(user_id: current_user.id, users: { is_deleted: true }).order('RAND()').limit(4)
+    @random = Tweet.joins(:user).where.not(user_id: current_user.id, users: { is_deleted: true }).order('RANDOM()').limit(4)
     @q = Tweet.joins(:user).where(users: { is_deleted: false }).ransack(params[:q])
     @results = @q.result(distinct: true)
     @tweets = Tweet.joins(:user).where(users: { is_deleted: false }).order(created_at: 'DESC')
@@ -16,6 +16,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
+    # syntax = Language.get_data(tweet_params[:body])
     if @tweet.save
       flash[:notice] = 'つぶやきを投稿しました'
       redirect_to tweets_path
@@ -35,7 +36,7 @@ class TweetsController < ApplicationController
   end
 
   def following_tweets
-    @random = Tweet.joins(:user).where.not(user_id: current_user.id, users: { is_deleted: true }).order('RAND()').limit(4)
+    @random = Tweet.joins(:user).where.not(user_id: current_user.id, users: { is_deleted: true }).order('RANDOM()').limit(4)
     @tweets = Tweet.joins(:user).where(users: { is_deleted: false }).where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: 'DESC')
     @q = Tweet.joins(:user).where(users: { is_deleted: false }).ransack(params[:q])
     @results = @q.result(distinct: true)
@@ -48,10 +49,11 @@ class TweetsController < ApplicationController
 
   def ranking
     range = Time.zone.today.in_time_zone.all_month
-    @favorites = Tweet.joins(:user).where(created_at: range)
-                            .find(TweetFavorite.joins(:user).where(users: { is_deleted: false }).where(created_at: range).group(:tweet_id).order('count(tweet_id) desc').limit(3).pluck(:tweet_id))
-    @comments = TweetComment.joins(:user).where(created_at: range)
-                            .find(CommentFavorite.joins(:user).where(users: { is_deleted: false }).where(created_at: range).group(:tweet_comment_id).order('count(tweet_comment_id) desc').limit(3).pluck(:tweet_comment_id))
+    @favorites = Tweet.joins(:user).joins(tweet_favorites: :user).where("users.is_deleted = ?", false).where("users_tweet_favorites.is_deleted = ?", false)
+    .group("tweet_favorites.tweet_id").order("count(tweet_id) desc").where(created_at: range).limit(3)
+    
+    @comments = TweetComment.joins(:user).joins(comment_favorites: :user).where("users.is_deleted = ?", false).where("users_comment_favorites.is_deleted = ?", false)
+    .group("comment_favorites.tweet_comment_id").order("count(tweet_comment_id) desc").where(created_at: range).limit(3)
   end
 
   private
